6eadc2bbf02bf4576d871ace8963d7548bd9d7ef
adamconkle <adam.conkle@gmail.com>
2024-09-28 20:53:36 -0700

    Create flexbox-style-modifier.js

---
+++ flexbox-style-modifier.js
@@ -0,0 +1,39 @@
+// Justify Content
+function spaceBetween() {    document.getElementById("exampleArea").style.justifyContent = "space-between"; 
+}
+
+function spaceAround() {    document.getElementById("exampleArea").style.justifyContent = "space-around"; 
+}
+
+// broken???
+function stretch() {    document.getElementById("exampleArea").style.justifyContent = "stretch"; 
+}
+
+function center() {    document.getElementById("exampleArea").style.justifyContent = "center"; 
+}
+
+function flexStart() {    document.getElementById("exampleArea").style.justifyContent = "flex-start"; 
+}
+
+function flexEnd() {    document.getElementById("exampleArea").style.justifyContent = "flex-end"; 
+}
+
+// Align Content
+function spaceBetweenAC() {    document.getElementById("exampleArea").style.alignContent = "space-between"; 
+}
+
+function spaceAroundAC() {    document.getElementById("exampleArea").style.alignContent = "space-around"; 
+}
+
+// broken???
+function stretchAC() {    document.getElementById("exampleArea").style.alignContent = "stretch"; 
+}
+
+function centerAC() {    document.getElementById("exampleArea").style.alignContent = "center"; 
+}
+
+function flexStartAC() {    document.getElementById("exampleArea").style.alignContent = "flex-start"; 
+}
+
+function flexEndAC() {    document.getElementById("exampleArea").style.alignContent = "flex-end"; 
+}
